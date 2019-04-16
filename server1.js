c5c95be62a3c11238742c8c3a5a4140c6f892c51
Andrew Chalenko <andrysha1999@gmail.com>
2019-04-16 11:16:17 +0100

    json

--- server1.js
+++ server1.js
@@ -1,4 +1,8 @@
 var http=require("http");
+var obj={
+login:"andry",
+age:20
+};
 //var app=connect().use(connect.static('public')).listen(3000,"0.0.0.0");
 http.createServer(function(request,response){
 	console.log("Url"+request.url);
@@ -6,7 +10,7 @@ http.createServer(function(request,response){
 	console.log("User-Agent:"+request.headers["user-agent"]);
 	console.log("Все заголовки");
 	console.log(request.headers);
-	response.setHeader("Content-type","text/html;charset=utf-8");
+	response.setHeader("Content-type","application/json");
 	response.write("<h2>Hello world</h2>");
-	response.end();
+	response.end(JSON.stringify(obj));
 }).listen(3000,"0.0.0.0",function(){console.log("Server started!")});
