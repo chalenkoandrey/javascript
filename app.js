7a27b27296698672519db3867346ff923004d27f
Andrew Chalenko <andrysha1999@gmail.com>
2019-05-20 18:24:06 +0300

    entity

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-var path = require('path');
 var express = require("express");
+var entity=require("entity");
+var UserEntity=entity.extend(function(){
+        this.a=1;
+})
+var userEnt=UserEntity.extend();
+entity.create({name:'fed'})
+        .then(function(udo){
+          udo.name === 'fed';      
+        })
+        .catch(function(error){
+          console.log("error");
+        });
 var app = express();
 var array=require("./arrayOfUsers");
 var max=10;
 var min=0;
 app.get("", (req, res, next) => {
         var i=Math.round(min+Math.random()*(max-min+1));
-        console.log("Случайное число"+i);
+        console.log("Случайное число="+i);
  res.json(array.arrayUsers[i].name);
 });
 app.listen(3000, () => {
