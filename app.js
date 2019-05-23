0232a66f60ae5a17ff605d953eaafd6e972f80ae
Andrew Chalenko <andrysha1999@gmail.com>
2019-05-23 15:01:49 +0300

    add return

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
 var express = require("express");
 var MongoClient = require('mongodb').MongoClient;
-var url="mongodb://localhost:27017/";
-var mongoclient=new MongoClient(url,{native_parser:true});
+var url = "mongodb://localhost:27017/";
+var mongoclient = new MongoClient(url, { useNewUrlParser: true });
 var app = express();
-app.use(express.urlencoded({extended: true}));
-mongoclient.connect(function(err, client){
-  if(err){
-    return console.log(err);
+app.use(express.urlencoded({ extended: true }));
+mongoclient.connect(function (err, client) {
+  if (err) {
+    return 1;
   }
-  var db=client.db("UserDB");
+  var db = client.db("UserDB");
   const collection = db.collection("UserDB");
-  app.post("/add", function(req, res) {
-    if(!req.body) return res.sendStatus(400);
-    console.log(req.body);
-    if(req.body.id)
-      {
-        let user = {"id":req.body.id ,name:req.body.name,date:req.body.date};
-        collection.insertOne(user, function(err, result){
-          if(err){ 
-            return console.log(err);
-          }
-      //console.log(result.ops);
-        });
-        collection.find().toArray(function(err,result){
-          res.json(result);
-        });
-      }        
+  app.post("/add", function (req, res) {
+    if (!req.body) return res.sendStatus(400);
+    if (req.body.id && req.body.name && req.body.date) {
+      let user = { "id": req.body.id, name: req.body.name, date: req.body.date };
+      collection.insertOne(user);
+      collection.find().toArray(function (err, result) {
+        return res.json(result);
+      });
+    }
+    else {
+      return res.send({ error: "Not full params" })
+    }
   });
   app.get("/show", (req, res, next) => {
-    collection.find().toArray(function(err,result){
-      res.json(result);
-      });
+    collection.find().toArray(function (err, result) {
+      return res.json(result);
+    });
   });
   app.delete("/delete", (req, res, next) => {
-    collection.deleteOne({"id":req.body.id},function(err, result){
-      if(err){ 
-        return console.log(err);
+    collection.find({ "id": req.body.id }).toArray(function (err, result) {
+      ;
+      if (!result[0]) {
+        return res.send({ error: "No many user with whis id" });
       }
-    }); 
-    collection.find().toArray(function(err,result){
-      res.json(result);
+      collection.deleteMany({ "id": req.body.id });
+      collection.find().toArray(function (err, result) {
+        return res.json(result);
       });
     });
+
+  });
   app.listen(3000, () => {
     console.log("Server running on port 3000");
-    });
+  });
 });
