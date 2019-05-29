91baceadeef005fab0ba28bbcb0d8806527c07dd
Andrew Chalenko <andrysha1999@gmail.com>
2019-05-29 12:31:06 +0300

    delete some code

--- route.js
+++ route.js
@@ -3,9 +3,8 @@ var router = express.Router();
 var controller = require("./contoroller");
 router.route("/users/")
   .post((req, res, next) => {
-    if (!req.body) return res.sendStatus(400);
     if (req.body.name && req.body.date) {
-      controller.addUser(req, res, req.body.name, req.body.date)
+      controller.addUser(req, res)
     }
     else {
       return res.send({ error: "Not full params" })
