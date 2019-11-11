4b68cf8dfea74f08d34d100d6e9df603d2fb2e13
Andrew Chalenko <andrysha1999@gmail.com>
2019-11-11 16:37:44 +0200

    change ports

--- server/app.js
+++ server/app.js
@@ -5,6 +5,6 @@ app.use(cors())
 const router = require("./route").Router;
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(9000, () => {
+  console.log("Server running on port 9000");
 });
