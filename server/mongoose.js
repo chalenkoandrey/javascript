0cf99f9ed06f0794c47dd938b85e3fe470335208
Andrew Chalenko <andrysha1999@gmail.com>
2019-11-11 17:53:19 +0200

    change url

--- server/mongoose.js
+++ server/mongoose.js
@@ -1,4 +1,4 @@
-const url = "mongodb://127.0.0.1:27017/UserDB";
+const url = "mongodb://mongo:27017/UserDB";
 const mongoose = require('mongoose');
 mongoose.connect(url, {
   useNewUrlParser: true,
